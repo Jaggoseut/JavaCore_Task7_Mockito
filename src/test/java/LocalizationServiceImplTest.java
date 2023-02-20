5e94919f26cad53f690c6d71d7d6189aaa7c8302
Jaggoseut <zhenyavolkov1995@yandex.ru>
2023-02-20 20:57:39 +0300

    Добавил тесты для LocalizationService

--- src/test/java/LocalizationServiceImplTest.java
+++ src/test/java/LocalizationServiceImplTest.java
@@ -1,2 +1,18 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.entity.Country;
+import ru.netology.i18n.LocalizationServiceImpl;
+
 public class LocalizationServiceImplTest {
+
+    @Test
+    void test_localization_service_impl() {
+        String expected = "Добро пожаловать";
+
+        LocalizationServiceImpl localizationService = new LocalizationServiceImpl();
+
+        String preferences = localizationService.locale(Country.RUSSIA);
+
+        Assertions.assertEquals(expected, preferences);
+    }
 }

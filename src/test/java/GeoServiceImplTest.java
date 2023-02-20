95ee5722e02f41c6e5237e53c521579640fbc120
Jaggoseut <zhenyavolkov1995@yandex.ru>
2023-02-20 21:01:56 +0300

    Добавил тест для GeoService

--- src/test/java/GeoServiceImplTest.java
+++ src/test/java/GeoServiceImplTest.java
@@ -1,2 +1,20 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.entity.Country;
+import ru.netology.entity.Location;
+import ru.netology.geo.GeoService;
+import ru.netology.geo.GeoServiceImpl;
+
 public class GeoServiceImplTest {
+
+    @Test
+    void test_geo_service() {
+
+        Location expect = new Location("Moscow", Country.RUSSIA, null, 0);
+        GeoServiceImpl geoService = new GeoServiceImpl();
+
+        Location preference = geoService.byIp("172.");
+
+        Assertions.assertEquals(expect, preference);
+    }
 }
